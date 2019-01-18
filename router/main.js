455e94b0aba714a8757a476f3c66a013c8bfcbf1
DalCotton <40486279+DalCotton@users.noreply.github.com>
2019-01-18 13:15:51 +0900

    Create main.js

---
+++ router/main.js
@@ -0,0 +1,51 @@
+
+var mysql = require('mysql');
+var client = mysql.createConnection({
+    user: "root",
+    password: "1234",
+    database: "mydb"
+});
+
+
+
+module.exports = function(app){
+    
+    //POST Parameters
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    //session
+    var expressSession = require('express-session');
+    app.use(expressSession({
+        secret: 'my key',
+        resave: true,
+        saveUninitialized: true
+    }));
+    
+
+    /* WEB URI */
+    //index
+    app.get('/', function(req, res){
+        if(req.session.user){
+            //
+            res.render('index.html');
+        }else{
+            res.render('index.html');
+        }
+    });
+    app.post('/', function(req, res, next){
+        //
+    });
+
+    //userInput(스마트컨트랙트)
+    app.get('/input', function(req, res){
+        //if(req.session.user){
+            //
+            res.render('input.html');
+        //}else{
+        //    console.log('need login!');
+        //    res.render('index.html');
+        //}
+    });
+}
