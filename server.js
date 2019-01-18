17429e670b0b023dc1cf5e9c3ef1f2fcec82c2b0
DalCotton <40486279+DalCotton@users.noreply.github.com>
2019-01-18 13:15:12 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//Router Module(main.js) >> app
+var router = require('./router/main') (app);
+
+//html위치정의(server html rendering),EJS engine setting. 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000.");
+});
+
+//정적파일(css,js,image)
+app.use(express.static('public'));
+
